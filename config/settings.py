c4b562fa85db2e2a453827862a213a75673dc022
Khabib Toshev <khabibtoshev@gmail.com>
2024-07-23 05:42:55 +0500

    songgi ozgarishlar

--- config/settings.py
+++ config/settings.py
@@ -147,7 +147,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = 'home/mywebsit/my-website.uz/django/media'
 
 #local
-#MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
